12673044db178f5795e71a25d542abbe3a7f2039
Muhammad Hanzala Khan <B23F0260AI095@paf-iast.edu.pk>
2026-07-06 12:01:30 +0500

    Implement dynamic dashboard using JSON data

--- js/navbar.js
+++ js/navbar.js
@@ -1,2 +1,51 @@
-// Navbar JavaScript
-console.log("Navbar Loaded Successfully");
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>50){
+
+header.classList.add("scrolled");
+
+}else{
+
+header.classList.remove("scrolled");
+
+}
+
+});
+
+const sections=document.querySelectorAll("section");
+
+const navLinks=document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll",()=>{
+
+let current="";
+
+sections.forEach(section=>{
+
+const top=section.offsetTop-120;
+
+const height=section.clientHeight;
+
+if(pageYOffset>=top){
+
+current=section.getAttribute("id");
+
+}
+
+});
+
+navLinks.forEach(link=>{
+
+link.classList.remove("active");
+
+if(link.getAttribute("href")==="#"+current){
+
+link.classList.add("active");
+
+}
+
+});
+
+});
